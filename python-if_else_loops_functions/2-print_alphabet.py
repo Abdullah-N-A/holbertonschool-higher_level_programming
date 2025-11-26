40927299f455e3384f1579e7ae29f0f9574c3cf2
Abdullah-N-A <abdullahttlrx@gmail.com>
2025-11-26 09:11:06 +0000

    new up

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-
-# Iterate through the ASCII codes for 'a' (97) up to 'z' (122).
-# The range function goes up to, but does not include, the second number,
-# so we use 123 to include the code for 'z'.
 for i in range(97, 123):
-    # Use the print function only once.
-    # "{:c}".format(i) converts the ASCII integer (i) into its corresponding character.
-    # end="" ensures no newline is printed, keeping the output on a single line.
     print("{:c}".format(i), end="")
