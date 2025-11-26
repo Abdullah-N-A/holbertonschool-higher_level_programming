33c21c7767555ace39809eb0ef60102473e1faca
Abdullah-N-A <abdullahttlrx@gmail.com>
2025-11-26 09:08:58 +0000

    new up

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    print("{:c}".format(i), end="")
+import string
+
+# Python's string module provides a constant for the lowercase alphabet.
+alphabet = string.ascii_lowercase
+
+# Print the alphabet string on a single line.
+# The 'end=""' argument suppresses the default newline character.
+print(alphabet, end="")
