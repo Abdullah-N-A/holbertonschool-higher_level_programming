413a326ea58141a3de2ca6b427504add0fe43a9a
Abdullah-N-A <abdullahttlrx@gmail.com>
2025-12-10 17:27:48 +0000

    new tskl

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Difines asquare class with a pivate size attribute."""
+
+
+
+class square:
+    """Represents a square."""
+
+
+
+    def __init__(self,size):
+       """initaialze the square with a private size."""
+       self.__size = size
