d2ea2614a74c8555c7ba4780666536880c0322c0
Abdullah-N-A <Abdullahttlrx@gmail.com>
2025-12-13 20:33:29 +0300

    Update 6-rectangle.py

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -58,8 +58,11 @@ class Rectangle:
         return "\n".join("#" * self.__width for _ in range(self.__height))
 
     def __repr__(self):
-        """Return a string representation that can recreate the instance."""
-        return "Rectangle({}, {})".format(self.__width, self.__height)
+    """Return a string representation that can recreate the instance."""
+    return "Rectangle({}, {})".format(
+        self.__width, self.__height
+    )
+
 
     def __del__(self):
         """Print a message when an instance is deleted and decrement counter."""
