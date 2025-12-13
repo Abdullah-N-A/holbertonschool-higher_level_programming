b511b9d2fb983924fe93c0b194d3c4d219308619
Abdullah-N-A <abdullahttlrx@gmail.com>
2025-12-13 18:43:13 +0300

    new file

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -4,10 +4,13 @@
 A module with a Rectangle that does nothing
 
 """
+
+
 class Rectangle:
     """
 
-       An empty Rectangle class
+    An empty Rectangle class
 
-       """
+    """
+
     pass
