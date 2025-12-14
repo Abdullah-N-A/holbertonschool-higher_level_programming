9b75a037633ec27842e5e577f450b5c41eaafd8c
Abdullah-N-A <abdullahttlrx@gmail.com>
2025-12-14 15:01:06 +0000

    new

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
+"""
+Module that defines a function to list available attributes and methods of an object
+"""
+
 def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object
+
+    Args:
+        obj: any Python object
+
+    Returns:
+        list: list of attributes and methods
+    """
     return dir(obj)
